71697b7a525b620fc70831e685a18d7a63fd391d
Daksh Kulshrestha <akshd@outlook.com>
2021-11-13 15:00:16 +0530

    feat: added commisions

--- contracts/Metapass.sol
+++ contracts/Metapass.sol
@@ -1,27 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Polytix is ERC721URIStorage, Ownable {
+contract Metapass is ERC721URIStorage, Ownable {
+    
+    
     using Counters for Counters.Counter;
+    uint256 public cutNumerator = 10;
+    uint256 public cutDenominator = 100;
 
     Counters.Counter private _tokenIdCounter;
     bool private isMinting = false;
-    mapping(address => uint256) owners;
+    mapping(address => uint256) balances;
     
     constructor() ERC721("Metapass", "MPA") {}
+
+  
+    function updateCut(uint256 numerator, uint256 denominator) onlyOwner public {
+        cutNumerator = numerator;
+        cutDenominator = denominator;
+    }
+  
 
     function getTix (address eventOwner, string memory tokenMetadata) payable public {
         require(isMinting == true, "Service not running");
         _safeMint(msg.sender, _tokenIdCounter.current());
         _setTokenURI(_tokenIdCounter.current(), tokenMetadata);
         _tokenIdCounter.increment();
-        owners[eventOwner] += msg.value;
+        uint256 cut = msg.value * cutNumerator / cutDenominator; 
+        balances[eventOwner] += msg.value - cut;
+        balances[owner()] += cut;
     }
     
     function _toggleMinting() onlyOwner public {
@@ -29,13 +42,13 @@ contract Polytix is ERC721URIStorage, Ownable {
     }
     
     function _claims() public view returns (uint256) {
-        return owners[msg.sender];
+        return balances[msg.sender];
     }
     
     function widthdraw() public {
-        uint256 amount = owners[msg.sender];
+        uint256 amount = balances[msg.sender];
         payable(msg.sender).transfer(amount);
-        owners[msg.sender] = 0;
+        balances[msg.sender] = 0;
     }
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-    const Polytix = await hre.ethers.getContractFactory("Polytix");
-    const polytixContract = await Polytix.deploy();
+    const Metapass = await hre.ethers.getContractFactory("Metapass");
+    const metapassContract = await Metapass.deploy();
 
-    await polytixContract.deployed();
+    await metapassContract.deployed();
 
-    console.log("Contract deployed to:", polytixContract.address);
+    console.log("Contract deployed to:", metapassContract.address);
 
-    let txn = await polytixContract._toggleMinting();
+    let txn = await metapassContract._toggleMinting();
     await txn.wait();
     console.log("Minting is on now")
-    // txn = await polytixContract.getTix("0x4006c21A130D70000f59e009E4f81DB18eb1Ef00", "{'metadata': 'test'}", {
-    //     value: 100,
-    // });
-    // await txn.wait();
-
-    // txn = await polytixContract._claims();
-    // await txn.wait();
-
 }
 
 
