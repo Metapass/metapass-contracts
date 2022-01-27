71bdfa1788704c895823a844ad3c14dd85d79645
Anoushk Kharangate <32778608+anoushk1234@users.noreply.github.com>
2022-01-27 13:41:44 +0530

    Update Metapass.sol

--- contracts/Metapass.sol
+++ contracts/Metapass.sol
@@ -16,21 +16,24 @@ contract Metapass is ERC721URIStorage, Ownable {
         
     address public eventHost;
     uint256 cost;
+    MetaStorage storageProxy;
 
-    constructor(uint256 _cutNum, uint256 _cutDen, address _owner, uint256 _cost) ERC721("MetapassTickets", "METAPASS") {
+    constructor(uint256 _cutNum, uint256 _cutDen, address _owner, uint256 _cost,address _storageProxy) ERC721("MetapassTickets", "METAPASS") {
         cutNumerator = _cutNum;
         cutDenominator = _cutDen;
         cost = _cost;
         eventHost = _owner;
+        storageProxy = MetaStorage(_storageProxy);
     }
 
-    function getTix (string memory tokenMetadata) payable public {
+    function getTix (string memory tokenMetadata,string memory title, uint256 fee, uint256 seats,uint256 occupiedSeats, string memory image, address eventHostAddress, string memory description, string memory link, string memory date) payable public {
         _safeMint(msg.sender, _tokenIdCounter.current());
         _setTokenURI(_tokenIdCounter.current(), tokenMetadata);
         _tokenIdCounter.increment();
         uint256 cut = msg.value * cutNumerator / cutDenominator;
         payable(eventHost).transfer(msg.value - cut);
         payable(owner()).transfer(cut);
+        storageProxy.pushEventDetails(title, fee, seats,occupiedSeats, image, eventHostAddress, description, link, date, address(child));
     }
 
     function getLastTokenId() public view returns(uint256) {
